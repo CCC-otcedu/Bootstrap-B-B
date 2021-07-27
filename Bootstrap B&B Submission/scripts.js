4cb43b626282c04bc345fb0c63faaa61a4247726
CCC-otcedu <86634328+CCC-otcedu@users.noreply.github.com>
2021-07-26 19:05:34 -0500

    Add files via upload

---
+++ Bootstrap B&B Submission/scripts.js
@@ -0,0 +1,1 @@
+document.getElementById("Footer").innerHTML = "<footer class='text-light bg-dark'>Thanks for considering our establishment! Enjoy your stay here at the Kaliua Kove Bed & Breakfast!</footer>"
